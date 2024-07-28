aeb359a29a0f817c9d581adc725824a8ce2a5073
pk <pradipkamble9948@gmail.com>
2024-07-28 23:40:50 +0530

    state management with redux

--- megaBlogaAppWrite/src/App.jsx
+++ megaBlogaAppWrite/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import conf from './conf/conf'
+import { useDispatch } from 'react-redux'
+import authservice from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(conf);
-  return (
-    <>
-    <h1>Blog with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading]= useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authservice.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login(userData))
+      }
+      else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ): null
 }
 
 export default App
